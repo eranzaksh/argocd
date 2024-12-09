60eec29de8b359f9b7698b9e216ea914e33c965e
eran.zaksh <eranzaksh@gmail.com>
2024-12-09 13:15:54 +0200

    Adding terraform to create eks in vpc with argocd and ingress

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -24,20 +24,3 @@ resource "aws_security_group" "lb_sg" {
     
   }
 }
-# resource "aws_security_group" "prod_sg" {
-#   vpc_id = var.vpc_id
-
-#   egress = {
-#     from_port = 0
-#     to_port = 0
-#     protocol = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-  
-#   ingress = {
-#     from_port = 5000
-#     to_port = 5000
-#     protocol = "tcp"
-#     security_groups = [aws_security_group.lb_sg.id]
-#   }
-# }
